c12cd03a261f9388757348cbd1214a5a8e6e8136
annepetersen1 <ahpe@sund.ku.dk>
2017-05-26 14:49:25 +0200

    Added simulated data examples to section 2. This is the final version for submission.

---
+++ ESS/sim_notes.R
@@ -0,0 +1,65 @@
+library(MASS)
+library(PCADSC)
+library(ggplot2)
+
+#Simulate data from covariance matrices
+#Note: Names (S1 and S2) are opposite of that in the article.
+
+set.seed(11234)
+
+S1 <- diag(6)
+S1[c(1, 2, 3), 1] <- S1[1, c(1, 2, 3)] <- c(1, 0.2, 0.1)
+S1[2, 3] <- S1[3, 2] <- 0.1
+S1[c(4, 5, 6), 4] <- S1[4, c(4, 5, 6)] <- c(1, 0.3, 0.1)
+S1[5, 6] <- S1[6, 5] <- 0.2
+
+S2 <- diag(6)
+S2[lower.tri(S2)] <- c(0, 0, 0, 0, 0,
+                                    0, 0, 0, 0,
+                                    0.7, 0, 0,
+                                    0, 0,
+                                    0.4)
+S2[upper.tri(S2)] <- t(S2)[upper.tri(t(S2))]
+
+
+d1 <- as.data.frame(mvrnorm(500, rep(0, 6), S1))
+d2 <- as.data.frame(mvrnorm(500, rep(0, 6), S2))
+
+dF1 <- rbind(d1, d2)
+dF1$group <- rep(c("Group 1", "Group 2"), each = 500)
+
+dF2 <- as.data.frame(mvrnorm(1000, rep(0,6), S2))
+dF2$group <- rep(c("Group 1", "Group 2"), each = 500)
+
+
+#Do PCADSC
+po1 <- PCADSC(dF1, "group")
+po2 <- PCADSC(dF2, "group")
+
+CE1 <- CEPlot(po1) +
+  scale_y_continuous(breaks = round(seq(-0.6, 0.6, 0.1),1), limits = c(-0.5, 0.3))
+
+CE2 <- CEPlot(po2) +
+  scale_y_continuous(breaks = round(seq(-0.6, 0.6, 0.1),1), limits = c(-0.3, 0.3))
+
+
+angle1 <- anglePlot(po1)
+angle2 <- anglePlot(po2)
+
+chroma1 <- chromaPlot(po1)
+chroma2 <- chromaPlot(po2)
+
+ggsave(plot = CE1, file = "./article/simCE1.pdf",
+       width = 8, height = 5)
+ggsave(plot = CE2, file = "./article/simCE2.pdf",
+       width = 8, height = 5)
+
+ggsave(plot = angle1, file = "./article/simAngle1.pdf",
+       width = 8, height = 5)
+ggsave(plot = angle2, file = "./article/simAngle2.pdf",
+       width = 8, height = 5)
+
+ggsave(plot = chroma1, file = "./article/simChroma1.pdf",
+       width = 8, height = 5)
+ggsave(plot = chroma2, file = "./article/simChroma2.pdf",
+       width = 8, height = 5)
